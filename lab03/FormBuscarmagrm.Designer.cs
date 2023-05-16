e3d7a82f56185bc47d9604856808903b2c86d17c
carlos.efspires <carlos.efspires@SBC0505243W10-1>
2023-05-16 10:40:04 -0300

    Adicionar arquivos de projeto.

---
+++ lab03/FormBuscarmagrm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace lab03
+{
+    partial class FormBuscarmagrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxscreen = new System.Windows.Forms.PictureBox();
+            this.openFileDialogbuscar = new System.Windows.Forms.OpenFileDialog();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxscreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxscreen
+            // 
+            this.pictureBoxscreen.Location = new System.Drawing.Point(27, -1);
+            this.pictureBoxscreen.Name = "pictureBoxscreen";
+            this.pictureBoxscreen.Size = new System.Drawing.Size(750, 316);
+            this.pictureBoxscreen.TabIndex = 0;
+            this.pictureBoxscreen.TabStop = false;
+            // 
+            // openFileDialogbuscar
+            // 
+            this.openFileDialogbuscar.FileName = "openFileDialogbuscar";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(301, 321);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(196, 40);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Buscar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // FormBuscarmagrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pictureBoxscreen);
+            this.MaximizeBox = false;
+            this.Name = "FormBuscarmagrm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscar Imagem";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxscreen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxscreen;
+        private OpenFileDialog openFileDialogbuscar;
+        private Button btn1;
+    }
+}
---
+++ lab03/FormBuscarmagrm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab03
+{
+    public partial class FormBuscarmagrm : Form
+    {
+        public FormBuscarmagrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            openFileDialogbuscar.Filter = "Arquivos bmp| *.bmp | Arquivos jpg |*.jpg";
+            openFileDialogbuscar.ShowDialog();
+            if(openFileDialogbuscar.ShowDialog() == DialogResult.OK )
+            {
+                pictureBoxscreen.ImageLocation = openFileDialogbuscar.FileName;
+            }
+        }
+    }
+}
